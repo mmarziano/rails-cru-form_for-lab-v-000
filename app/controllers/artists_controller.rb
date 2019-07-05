8eec78e72868f8979b853a4f6ce37760e1770349
Monica Marziano <mmarziano1@gmail.com>
2019-07-05 00:38:38 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,22 @@
 class ArtistsController < ApplicationController
+
+  def index 
+    @artists = Artist.all 
+  end 
+
+  def new 
+    @artist = Artist.new 
+  end 
+
+  def create 
+
+  end 
+
+  def edit 
+    @artist = Artist.find(params[:id])
+  end 
+
+  def update 
+
+  end 
 end
