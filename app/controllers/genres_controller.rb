7746bf09136557f7ddd4610dff88808dd8a21dfa
Monica Marziano <mmarziano1@gmail.com>
2019-07-05 00:37:36 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,22 @@
 class GenresController < ApplicationController
+
+  def index 
+    @genres = Genre.all 
+  end 
+
+  def new 
+    @genre = Genre.new 
+  end 
+
+  def create 
+
+  end 
+
+  def edit 
+    @genre = Genre.find(params[:id])
+  end 
+
+  def update 
+
+  end 
 end
